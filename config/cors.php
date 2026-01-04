460c92a75ea6d93ec186cc8ad22d5f79adb8398b
Ahmed Butt <imahmedthebutt@gmail.com>
2026-01-04 18:56:17 +0500

    first commit

---
+++ config/cors.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'login',
+        'logout',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost',
+        'http://127.0.0.1:8003',
+        'https://photoappfrontend.z36.web.core.windows.net',
+        'https://photoapp-api-ash4f3d4gacsh7hp.polandcentral-01.azurewebsites.net/',
+        'https://photoapp-api-ash4f3d4gacsh7hp.polandcentral-01.azurewebsites.net/api',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
+
